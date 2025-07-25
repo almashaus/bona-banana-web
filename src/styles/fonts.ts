abfec74e50033bb4f7a9e20f5b322c2e5a95852a
hadeel-harbi <ihadeel_abdullah@hotmail.com>
2025-07-25 19:20:10 +0300

    edit fonts name

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,27 +3,27 @@ import localFont from "next/font/local";
 export const DINNextLT = localFont({
   src: [
     {
-      path: "../../public/font/din-next-lt/DINNextLTArabic-UltraLight.ttf",
+      path: "../../public/font/din-next-lt/DINNextLT-UltraLight.ttf",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../public/font/din-next-lt/DINNextLTArabic-Light.ttf",
+      path: "../../public/font/din-next-lt/DINNextLT-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../public/font/din-next-lt/DINNextLTArabic-Regular.ttf",
+      path: "../../public/font/din-next-lt/DINNextLT-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/font/din-next-lt/DINNextLTArabic-Medium.ttf",
+      path: "../../public/font/din-next-lt/DINNextLT-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/font/din-next-lt/DINNextLTArabic-Bold.ttf",
+      path: "../../public/font/din-next-lt/DINNextLT-Bold.ttf",
       weight: "700",
       style: "normal",
     },
